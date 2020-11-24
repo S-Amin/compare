8e94f8c9951477db22cdf685c2bd6e1e0d6a56f1
Saeed Amin <s4ied.amin@gmail.com>
2020-11-24 13:16:55 +0100

    add link to compare page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <h1 style={{ textAlign: "center" }}>
       <div>Created by the React.js, Typescript</div>
       <br />
+      <Link to="/compare">Compare Product Page</Link>
     </h1>
   );
 };
